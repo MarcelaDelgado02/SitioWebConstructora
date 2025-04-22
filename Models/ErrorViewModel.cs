ddd874fd165f0a0a90e2ad73c338e61c1c5148c7
admar <admar@MARCELA>
2025-04-21 20:28:43 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SitioWebConstructora.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
