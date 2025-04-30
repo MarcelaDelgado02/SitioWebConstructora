5e22e848f1855c4c3b36ade076688cb83c922381
Marcela Abarca delgado <admarcela5@gmail.com>
2025-04-29 19:33:20 -0600

    Se crea el apartado de filtado y se hace la estructura de la tabla falta la lib datatabla

---
+++ Controllers/FiltradoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SitioWebConstructora.Controllers
+{
+    public class FiltradoController : Controller
+    {
+        public IActionResult Filtrado()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/assets/js/Filtrado de tablas_JavaScript.js
@@ -0,0 +1,46 @@
+﻿jsTablaFiltrado = {
+
+    objetos: {
+        
+
+    },
+    controles: {
+
+        
+
+    },
+
+    botones: {
+
+
+    },
+
+    variables: {
+
+
+
+
+    },
+    metodos: {
+
+
+
+
+    },
+    eventos:
+        function () {
+
+            $(jslogin.botones.btnIniciarSesion).on('click', function () {
+
+                jslogin.metodos.IniciarSesion();
+
+            });
+
+
+        }
+
+}
+
+$(function () {
+    jslogin.eventos();
+});
