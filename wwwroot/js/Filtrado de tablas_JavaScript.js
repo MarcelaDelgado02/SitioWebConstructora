e36847f5b59a8452ce332b1cf867be337e5edb89
Marcela Abarca delgado <admarcela5@gmail.com>
2025-04-30 19:15:00 -0600

    Se hace el filtro ya esta listo

---
+++ wwwroot/js/Filtrado de tablas_JavaScript.js
@@ -0,0 +1,30 @@
+﻿jsTablaFiltrado = {
+    objetos: {},
+    controles: {
+        TbDoc: "#TbDoc"
+    },
+    botones: {},
+    variables: {},
+    metodos: {
+        IniciarDataTableDocs: function () {
+            $(jsTablaFiltrado.controles.TbDoc).DataTable({
+
+                paging: false,
+                searching: true,
+                ordering: true,
+                info: false,
+                pageLength: 1,
+                lengthMenu: [[2, 8, 14], [1, 2, 3]],
+                language: {
+                    search: "&nbsp Buscar bonos:&nbsp;"  
+                   
+                },
+            });
+        }
+    },
+    eventos: {} 
+};
+
+$(function () {
+    jsTablaFiltrado.metodos.IniciarDataTableDocs();
+});
---
+++ wwwroot/js/LayoutJavaScript.js
@@ -0,0 +1,62 @@
+﻿var jsResponsiveNav = {
+    objetos: {},
+    controles: {
+        navToggle: '.responsive-nav .nav-toggle',
+        navMenu: '.responsive-nav .nav-menu'
+    },
+    botones: {},
+    variables: {
+        isOpen: false
+    },
+    metodos: {
+        /**
+         * Inicializa la navegación responsiva: 
+         * - Controla el toggle del menú
+         * - Alinea el menú a la izquierda cuando se despliega
+         */
+        InitResponsiveNav: function () {
+            var self = this;
+            var $btn = $(self.controles.navToggle);
+            var $menu = $(self.controles.navMenu);
+            var $iconOpen = $btn.find('.icon-expand');
+            var $iconClose = $btn.find('.icon-close');
+
+            // Al clicar el botón hamburguesa
+            $btn.on('click', function () {
+                self.variables.isOpen = !self.variables.isOpen;
+                $menu.toggleClass('open', self.variables.isOpen);
+
+                // Mostrar/ocultar íconos
+                $iconOpen.toggle(!self.variables.isOpen);
+                $iconClose.toggle(self.variables.isOpen);
+
+                // Alinear menú a la izquierda
+                if (self.variables.isOpen) {
+                    $menu.css({
+                        'text-align': 'left',
+                        'padding-left': '1rem'
+                    });
+                } else {
+                    // Opcional: limpiar estilos inline al cerrarse
+                    $menu.css({
+                        'text-align': '',
+                        'padding-left': ''
+                    });
+                }
+            });
+        }
+    },
+    eventos: {
+        /**  
+         * Registra todos los eventos necesarios al cargar la página
+         */
+        Registrar: function () {
+            jsResponsiveNav.metodos.InitResponsiveNav();
+        }
+    }
+};
+
+// Al cargar documento
+$(function () {
+    jsResponsiveNav.eventos.Registrar();
+});
