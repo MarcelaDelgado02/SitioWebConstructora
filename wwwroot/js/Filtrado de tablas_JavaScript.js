f817f90e0f43318be73a173171561aa286510a85
Marcela Abarca delgado <admarcela5@gmail.com>
2025-05-20 10:49:52 -0600

    El correo esta listo estos cambios se subiran a somee 1ª Generaciòn

--- wwwroot/js/Filtrado de tablas_JavaScript.js
+++ wwwroot/js/Filtrado de tablas_JavaScript.js
@@ -16,8 +16,8 @@
                 lengthMenu: [[2, 8, 14], [1, 2, 3]],
                 language: {
                     search: "&nbsp;Buscar bonos:&nbsp;",
-                    zeroRecords: "Hola, no encontramos resultados con esos filtros", // Mensaje personalizado
-                    emptyTable: " Bono no encontrado: Verifique el nombre " // Mensaje cuando no hay datos iniciales
+                    zeroRecords: "Bono no encontrado: Verifique el nombre", // Mensaje personalizado
+                   
                 }
             });
         }
